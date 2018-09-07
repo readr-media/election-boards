e5426f03c4100efaab8fdadf70a605b8be15abd1
I-Chiao Hsu <ichiao.hsu@gmail.com>
2018-09-07 18:47:04 +0800

    candidates_terms, boards API modification 1. candidates_term support embedded latest boards 2. Default sorting: candidates_terms -> id, boards -> -uploaded_at 3. boards modified fields to fit spec

--- boards/models.py
+++ boards/models.py
@@ -2,15 +2,25 @@
 import uuid
 
 from django.contrib.gis.db import models
-from candidates.models import Candidates
+from candidates.models import Terms
 
 # Create your models here.
 class Boards(models.Model):
     # 必填 照片連結，座標，上傳者
-    address = models.CharField(max_length=150, blank=True)
     image = models.URLField(null=False, blank=False)
-    candidates = models.ManyToManyField(Candidates, related_name='board_candidates')
     coordinates = models.PointField(null=False)
+    
+    county = models.CharField(max_length=15, blank=True)
+    district = models.CharField(max_length=15, blank=True)
+    road = models.CharField(max_length=75, blank=True)
+    
+    candidates = models.ManyToManyField(Terms, related_name='boards')
+
     took_at = models.DateTimeField(null=True)
     uploaded_at = models.DateTimeField(null=True, auto_now_add=True)
-    uploader = models.UUIDField(unique=True, default=uuid.uuid4, editable=False, blank=False, null=False)
+    uploaded_by = models.UUIDField(unique=True, default=uuid.uuid4, editable=False, blank=False, null=False)
+
+    verified_amount = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-uploaded_at']
--- candidates/serializers.py
+++ candidates/serializers.py
@@ -1,14 +1,40 @@
 from rest_framework import serializers
 from .models import Terms, Candidates
+from boards.models import Boards
 
 # Create your views here.
 class CandidatesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidates
         fields = '__all__'
-        
+
+# special TermsSerializer for Boards
+class SingleBoardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Boards
+        fields = ('image','coordinates','county','district','road','took_at','uploaded_at','uploaded_by','verified_amount')
+
+    def to_representation(self, instance):
+        location = instance.coordinates
+        ret = super(SingleBoardSerializer, self).to_representation(instance)
+        del ret['coordinates']
+        ret['coordinates'] = '({},{})'.format(location[0],location[1])
+        return ret
+
 class CandidatesTermsSerializer(serializers.HyperlinkedModelSerializer):
+    boards = SingleBoardSerializer(many=True)
     class Meta:
         model = Terms
-        fields = '__all__'
+        fields = ('uid', 'election_year', 'type', 'county', 'district', 'name', 'party', 'number', 'image','boards')
+    
+    def to_representation(self, instance):
+        ret = super(CandidatesTermsSerializer, self).to_representation(instance)
+        if len(ret['boards']) > 1:
+            ret['boards'] = ret['boards'][0]
+        return ret
+
+class g0vCandidatesTermsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Terms
+        fields = ('uid', 'election_year', 'type', 'county', 'district', 'name', 'party', 'number', 'image')
 
