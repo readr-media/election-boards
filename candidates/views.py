66117d2f12b76342e972766af42645f4bab5ae2e
I-Chiao Hsu <ichiao.hsu@gmail.com>
2018-10-08 18:42:00 +0800

    Fix issue with verified_amount & not_board_amount filter 1. In previous version, filter functions return qeuryset directly from Terms objects. This would cause base filters to be invalid. Now in the filter function I modified the queryset instead of replace. So the base filters could be kept.
    2. Fix "not returning queryset" issue in verfied_amount filter

--- candidates/views.py
+++ candidates/views.py
@@ -7,9 +7,7 @@ from .filters import TermsFilter
 from app.filters import MaxResults
 
 class CandidatesTermsViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Terms.objects.select_related('candidate') \
-    .prefetch_related(Prefetch('boards', queryset=Boards.objects.annotate(not_board_amount=Count('board_checks', filter=Q(board_checks__type=2, board_checks__is_board=False))).filter(not_board_amount__lte=2).order_by('-uploaded_at'))) \
-    .annotate(verified_board_amount=Count('boards', filter=Q(boards__verified_amount__gte=3)))
+    queryset = Terms.objects.select_related('candidate')
     serializer_class = CandidatesTermsSerializer
     filter_fields = ('election_year', 'type', 'name', 'gender', 'party', 'constituency', 'county', 'district', 'votes', 'elected', 'occupy', 'verified_board_amount')
     filterset_class = TermsFilter
