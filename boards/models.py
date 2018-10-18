c8582b433e36b1b19aa5a53a79bcfe17964d107b
I-Chiao Hsu <ichiao.hsu@gmail.com>
2018-10-18 17:50:40 +0800

    Recorded boards creation
    Changelog
    1. headcount field set to nullable, and no default.
    In the update process for single checks, if the most frequent headcount
    is None, which means most validations do not include headcount info, we use
    the number of candidates when the board was inserted.
    
    If the headcount is 0, which means most frequent headcount think the
    candidates are out of our investigative range, we do not about original
    relation between boards and candidates.
    
    If headcount > 0, then we update the board-candidates relation, just
    like previous commits.
    
    2. Record the inserted boards in boards_checks
    When inserting new boards, the board data will be recorded in 'check'
    table as well to avoid losing the original description about this board.
    This table acts as a history record table.
    
    New field 'is_original' was introduced to distinguish whether this
    record is for brand new insertion or a validation.
    
    Migrations are made to record the original data for existing boards.

--- boards/models.py
+++ boards/models.py
@@ -10,11 +10,11 @@ class Boards(models.Model):
 
     image = models.CharField(max_length=256, null=False, blank=False)
     coordinates = models.PointField(null=False)
-    
+
     county = models.CharField(max_length=15, blank=True)
     district = models.CharField(max_length=15, blank=True)
     road = models.CharField(max_length=75, blank=True)
-    
+
     candidates = models.ManyToManyField(Terms, related_name='boards')
 
     took_at = models.DateTimeField(null=True)
@@ -42,9 +42,11 @@ class Checks(models.Model):
     candidates = models.ManyToManyField(Terms, related_name='check_candidates')
     slogan = models.CharField(max_length=128, null=True)
     is_board = models.BooleanField(default=False, null=False)
-    type = models.IntegerField(null=False, default=0)
-    created_by = models.UUIDField(editable=False, blank=False, null=False)
+    type = models.IntegerField(null=False, default=0)  # Check type: 1=single check, 2=multiple check
+
+    headcount = models.IntegerField(null=True, default=None)
+    is_original = models.BooleanField(null=True, default=False)
 
     created_at = models.DateTimeField(null=False, auto_now_add=True)
-    headcount = models.IntegerField(null=False, default=1)
+    created_by = models.UUIDField(editable=False, blank=False, null=False)
 
--- boards/serializers.py
+++ boards/serializers.py
@@ -6,14 +6,14 @@ from django.contrib.gis.geos import Point
 from django.db.models import Max, Count
 import re
 
-class TimestampField(serializers.DateTimeField): 
-    
+class TimestampField(serializers.DateTimeField):
+
     def to_internal_value(self, value):
         converted_time = datetime.fromtimestamp(float(value))
         return super(TimestampField, self).to_internal_value(converted_time)
-        
+
 class CoordinatesField(serializers.Field):
-    
+
     def to_internal_value(self, data):
         m = re.match(r'\((?P<lat>[0-9\.]+)\s+(?P<lon>[0-9\.]+)\)', data)
         return Point(float(m.group('lat')), float(m.group('lon')))
@@ -24,16 +24,16 @@ class CoordinatesField(serializers.Field):
 class BoardsTermsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Terms
-        fields = ('uid', 'election_year', 'type', 'county', 'district', 'name', 'party', 'number', 'image')
+        fields = ('id', 'uid', 'election_year', 'type', 'county', 'district', 'name', 'party', 'number', 'image')
 
 class MultiBoardsSerializer(serializers.ModelSerializer):
-    candidates = BoardsTermsSerializer(many=True,read_only=True)
+    candidates = BoardsTermsSerializer(many=True, read_only=True)
     coordinates = CoordinatesField()
 
     class Meta:
         model = Boards
         fields = '__all__'
-    
+
 class MultiBoardsDeserializer(serializers.ModelSerializer):
     candidates = serializers.PrimaryKeyRelatedField(many=True, queryset=Terms.objects.all())
     took_at = TimestampField()
@@ -43,15 +43,20 @@ class MultiBoardsDeserializer(serializers.ModelSerializer):
     class Meta:
         model = Boards
         fields = '__all__'
-    
+
     def create(self, validated_data):
-        # Create uploader
+        # Create a board
         board_candidates = validated_data.pop('candidates')
         board = Boards.objects.create(**validated_data)
+
+        check = Checks(board=validated_data.get('id',0), is_board=True, is_original=True, headcount=len(board_candidates), created_at = validated_data['uploaded_at'], created_by=validated_data['uploaded_by'])
+        check.save()
 
         # Create relationship
         for candidate in board_candidates:
             board.candidates.add(candidate)
+            check.candidates.add(candidate)
+
         return board
 
 class SingleCheckDeserializer(serializers.ModelSerializer):
@@ -60,13 +65,13 @@ class SingleCheckDeserializer(serializers.ModelSerializer):
     candidates = serializers.PrimaryKeyRelatedField(many=True, queryset=Terms.objects.all(), required=False)
     created_by = serializers.UUIDField(format='hex_verbose')
     is_board = serializers.BooleanField(required=True)
-    
+
     class Meta:
         model = Checks
         fields = '__all__'
 
     def create(self, validated_data):
-        check_candidates = [] 
+        check_candidates = []
         if 'candidates' in validated_data:
             check_candidates = validated_data.pop('candidates')
             # if len(check_candidates) <= 0:
@@ -75,30 +80,34 @@ class SingleCheckDeserializer(serializers.ModelSerializer):
         check = Checks.objects.create(**validated_data)
 
         # If there are denoted candidates, create relationship
-        if check_candidates: 
+        if check_candidates:
             for candidate in check_candidates:
                 check.candidates.add(candidate)
-        
+
         # Update verified_amount in board table
         check.board.verified_amount += 1
-        check.board.save() 
-       
-        # Select most headcount number 'headcount_max'
-        headcount_max = Checks.objects.filter(board=validated_data['board']).aggregate(Max('headcount'))
+        check.board.save()
 
+        # Select most headcount number 'headcount_max'
+        # If headcount_max is None, then choose the original length
+        headcount_max = Checks.objects.filter(board=validated_data['board'], is_original=False).aggregate(Max('headcount'))
         headcount_max = headcount_max['headcount__max']
 
+        if headcount_max is None:
+            headcount_max = Checks.objects.filter(board=validated_data['board'], is_original=True).only('headcount')[0].headcount
         # Select 'headcount_max' most frequent candidates in check
         most_candidates = [p['candidates__id'] for p in Checks.objects.filter(board=validated_data['board']) \
             .values('candidates__id').annotate(hcm=Count('candidates__id')).order_by('-hcm') \
             if p['candidates__id'] is not None and p['hcm'] != 0]
-        if len(most_candidates) > headcount_max:
-            most_candidates = most_candidates[:headcount_max]
-        # Update boards_boards_candidates to connect this boards with only most
-        # frequent candidates
-        validated_data['board'].candidates.clear()
-        for candidate in most_candidates:
-            validated_data['board'].candidates.add(candidate)
+
+        if headcount_max > 0:
+            if len(most_candidates) > headcount_max:
+                most_candidates = most_candidates[:headcount_max]
+            # Update boards_boards_candidates to connect this boards with only most
+            # frequent candidates
+            validated_data['board'].candidates.clear()
+            for candidate in most_candidates:
+                validated_data['board'].candidates.add(candidate)
         return check
 
 class MultiChecksDeserializer(serializers.Serializer):
@@ -117,7 +126,7 @@ class MultiChecksDeserializer(serializers.Serializer):
         valid_boards = validated_data.get('is_board', [])
         invalid_boards = validated_data.get('not_board', [])
         created_by = validated_data.get('created_by','')
-        
+
         if valid_boards:
             for board in valid_boards:
                 check = Checks(**{'board':board, 'is_board': True, 'created_by': created_by, 'type': 2})
